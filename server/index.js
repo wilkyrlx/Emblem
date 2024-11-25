f74f4964a5e7adf5e14327b7c6564401da19db52
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2024-11-25 17:08:39 -0500

    webRPC server 1

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const cors = require("cors");
+const server = http.createServer(app);
+const socket = require("socket.io");
+app.use(cors()); 
+
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
+
+const rooms = {};
+
+io.on("connection", socket => {
+    socket.on("join room", roomID => {
+        if (rooms[roomID]) {
+            rooms[roomID].push(socket.id);
+        } else {
+            rooms[roomID] = [socket.id];
+        }
+        const otherUser = rooms[roomID].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.emit("other user", otherUser);
+            socket.to(otherUser).emit("user joined", socket.id);
+        }
+    });
+
+    socket.on("offer", payload => {
+        io.to(payload.target).emit("offer", payload);
+    });
+
+    socket.on("answer", payload => {
+        io.to(payload.target).emit("answer", payload);
+    });
+
+    socket.on("ice-candidate", incoming => {
+        io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+    });
+});
+
+
+server.listen(8000, () => console.log('server is running on port 8000'));
